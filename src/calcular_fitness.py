12f5d5b1763d39622c5d39392f94705c3accc9cb
T0T11 <tomas.orellano@gmail.com>
2025-12-11 10:38:02 +0100

    fix: inicializar variable

--- src/calcular_fitness.py
+++ src/calcular_fitness.py
@@ -1,5 +1,6 @@
 from parametros import *
 def calcular_fitness(individuo,solucion):
+    fitness = 0
     for i in range(len(individuo)):
         if individuo[i] == solucion[i]:
             fitness += 2
