3af2dc688a1e1aaef421e1bfc4dc2574189cdf35
T0T11 <tomas.orellano@gmail.com>
2025-12-11 10:37:14 +0100

    fix: arreglar problema con la lista

--- src/calcular_fitness.py
+++ src/calcular_fitness.py
@@ -1,6 +1,6 @@
 from parametros import *
 def calcular_fitness(individuo,solucion):
-    for i in range(individuo):
+    for i in range(len(individuo)):
         if individuo[i] == solucion[i]:
             fitness += 2
         elif individuo[i] in solucion:
