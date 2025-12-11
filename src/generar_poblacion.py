0952009a00bc27762910fb080643f3ac6bbb9105
T0T11 <tomas.orellano@gmail.com>
2025-12-11 08:57:59 +0100

    feat: crear modulo para generar población

--- src/generar_poblacion.py
+++ src/generar_poblacion.py
@@ -1,7 +1,5 @@
 import random
-from main import *
+from parametros import *
 
 def generar_población():
-    return [[random.choice(main.COLORES, TAMAÑO_CODIGO)] for i in range(main.TAMAÑO_POBLACION)]
-
-print(generar_población)
+   return [[random.choice(COLORES)for i in range(TAMAÑO_CODIGO) ] for i in range(TAMAÑO_POBLACION)]
