3da32e79106138ac78e3de6df4912c160b4b39f7
T0T11 <tomas.orellano@gmail.com>
2025-12-11 09:38:28 +0100

    fix: elimine tilde

--- src/generar_poblacion.py
+++ src/generar_poblacion.py
@@ -1,5 +1,5 @@
 import random
 from parametros import *
 
-def generar_población():
+def generar_poblacion():
    return [[random.choice(COLORES)for i in range(TAMAÑO_CODIGO) ] for i in range(TAMAÑO_POBLACION)]
