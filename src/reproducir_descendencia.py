49a25c297fb91398610c618da200e459e322ae60
jrgeepd <pazosdominguez8@gmail.com>
2025-12-12 14:16:12 +0100

    feat: programar modulo de producir descendencia

---
+++ src/reproducir_descendencia.py
@@ -0,0 +1,22 @@
+import random
+from parametros import *
+def cruzar(padre1, padre2):
+    punto_cruce = len(padre1) // 2
+    return padre1[:punto_cruce] + padre2[punto_cruce:]
+
+def mutar(individuo):
+    mutado = individuo[:]
+    for i in range(len(mutado)):
+        if random.random() < TASA_MUTACION:
+            opciones = [c for c in COLORES if c != mutado[i]]
+            mutado[i] = random.choice(opciones)
+    return mutado
+
+def reproducir_descendencia(padres, num_hijos):
+    descendencia = []
+    for _ in range(num_hijos):
+        padre1, padre2 = random.sample(padres, 2)
+        hijo = cruzar(padre1, padre2)
+        hijo = mutar(hijo)
+        descendencia.append(hijo)
+    return descendencia
