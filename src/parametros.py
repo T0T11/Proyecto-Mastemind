671678d1a7b926f766521bfa74b83107fa9c0686
jrgeepd <pazosdominguez8@gmail.com>
2025-12-15 13:04:31 +0100

    feat: añadir el numero de intentos a los parametros

--- src/parametros.py
+++ src/parametros.py
@@ -2,4 +2,5 @@ COLORES = ['negro','rojo','azul','amarillo','blanco','verde']
 TASA_MUTACION = 0.1
 TAMAÑO_CODIGO = 4
 TAMAÑO_POBLACION = 100
+NUMERO_INTENTOS = 14
 num_padres = TAMAÑO_POBLACION // 2
