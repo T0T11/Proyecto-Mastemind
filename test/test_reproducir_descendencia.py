b4ae7c8a8ae6b2ab444cb7d82b383ef9e03210af
jrgeepd <pazosdominguez8@gmail.com>
2025-12-15 12:59:50 +0100

    test: hacer tet para las tres funciones del modulo

--- test/test_reproducir_descendencia.py
+++ test/test_reproducir_descendencia.py
@@ -1,1 +1,56 @@
 import pytest
+from src.parametros import *
+from src.reproducir_descendencia import *
+
+def test_reproducir_descendencia_num_hijos():
+    padres = [
+        ["negro", "rojo", "azul", "amarillo"],
+        ["blanco", "verde", "rojo", "azul"]
+    ]
+    hijos = reproducir_descendencia(padres, 2)
+    assert len(hijos) == 2
+
+def test_reproducir_descendencia_longitud_hijo():
+    padres = [
+        ["negro", "rojo", "azul", "amarillo"],
+        ["blanco", "verde", "rojo", "azul"]
+    ]
+    hijos = reproducir_descendencia(padres, 2)
+    for hijo in hijos:
+        assert len(hijo) == TAMAÑO_CODIGO
+
+def test_reproducir_descendencia_colores_validos():
+    padres = [
+        ["negro", "rojo", "azul", "amarillo"],
+        ["blanco", "verde", "rojo", "azul"]
+    ]
+    hijos = reproducir_descendencia(padres, 2)
+    for hijo in hijos:
+        for color in hijo:
+            assert color in COLORES
+
+def test_cruzar_a_mitad():
+
+    padre1 = ["negro", "rojo", "azul", "amarillo"]
+    padre2 = ["blanco", "verde", "rojo", "azul"]
+
+    copia1 = padre1[:]
+    copia2 = padre2[:]
+
+    _ = cruzar(padre1, padre2)
+
+    assert padre1 == copia1
+    assert padre2 == copia2
+
+
+def test_mutar():
+    individuo = ["negro", "rojo", "azul", "amarillo"]
+    
+    mutado = mutar(individuo)
+
+    assert isinstance(mutado, list)
+
+    assert len(mutado) == TAMAÑO_CODIGO
+
+    for color in mutado:
+        assert color in COLORES
