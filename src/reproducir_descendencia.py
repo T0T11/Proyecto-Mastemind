18405daba4360582d4deb9f355f76326520ee3e1
T0T11 <tomas.orellano@gmail.com>
2025-12-31 18:34:25 +0100

    refactor: :recycle: añadir un hijo mas a la funcion

--- src/reproducir_descendencia.py
+++ src/reproducir_descendencia.py
@@ -3,7 +3,9 @@ from src.parametros import *
 
 def cruzar(padre1, padre2):
     punto_cruce = len(padre1) // 2
-    return padre1[:punto_cruce] + padre2[punto_cruce:]
+    hijo1 = padre1[:punto_cruce] + padre2[punto_cruce:]
+    hijo2 = padre1[punto_cruce:] + padre2[:punto_cruce]
+    return hijo1, hijo2
 
 def mutar(cromosoma):
     mutado = cromosoma[:]
@@ -17,7 +19,12 @@ def reproducir_descendencia(padres, num_hijos):
     descendencia = []
     for _ in range(num_hijos):
         padre1, padre2 = random.sample(padres, 2)
-        hijo = cruzar(padre1, padre2)
-        hijo = mutar(hijo)
-        descendencia.append(hijo)
+
+        hijo1 , hijo2 = cruzar(padre1, padre2)
+
+        hijo1 = mutar(hijo1)
+        hijo2 = mutar(hijo2)
+
+        descendencia.append(hijo1)
+        descendencia.append(hijo2)
     return descendencia
